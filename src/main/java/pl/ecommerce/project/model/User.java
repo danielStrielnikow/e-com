4ddd16ac165fed377ba65fec3af8824933e234fb
Daniel <daniel.strielnikow@outlook.com>
2025-01-27 21:47:18 +0100

    Managing User Progile and Roles

--- src/main/java/pl/ecommerce/project/model/User.java
+++ src/main/java/pl/ecommerce/project/model/User.java
@@ -58,7 +58,7 @@ public class User {
     @Setter
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "user_address",
-            joinColumns = @JoinColumn(name = "user-id"),
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
 
