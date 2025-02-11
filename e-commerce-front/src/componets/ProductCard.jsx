dadc92e9da13bee509600f696cab7743ecb39f75
Daniel <daniel.strielnikow@outlook.com>
2025-02-11 18:27:08 +0100

    Product styles

--- e-commerce-front/src/componets/ProductCard.jsx
+++ e-commerce-front/src/componets/ProductCard.jsx
@@ -1,7 +1,34 @@
-const ProductCard = () => {
+import { useState } from "react";
+
+const ProductCard = ({
+    productId,
+    productName,
+    image,
+    description,
+    quantity,
+    price,
+    discount,
+    specialPrice,
+}) => {
+    const [openProductViewModal, setOpenProductViewModal] = useState(false);
+    const btnLoader = false;
+    const [selectViewProduct, setSelectViewProduct] = useState("");
+    const isAvailable = quantity && Number(quantity) > 0;
+
     return (
-        <div>
-            <h1>Product</h1>
+        <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
+            <div onClick={() => {}} className="w-full overflow-hidden aspect-[3/2]">
+                <img className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
+                src={image}
+                alt={productName}>
+                </img>
+            </div>
+            <div className="p-4 ">
+                <h2 onClick={() => {}}
+                    className="text-lg font-semibold mb-2 cursor-pointer">
+                    {productName}
+                </h2>
+            </div>
         </div>
     )
 }
