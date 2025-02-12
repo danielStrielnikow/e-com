43ed5ac884d21997b9d8ef2ec11887e306cbc69d
Daniel <daniel.strielnikow@outlook.com>
2025-02-12 19:43:28 +0100

    Add life to Product Filter

--- e-commerce-front/src/componets/Filter.jsx
+++ e-commerce-front/src/componets/Filter.jsx
@@ -6,8 +6,9 @@ import {
   Tooltip,
   Button,
 } from "@mui/material";
-import { useState } from "react";
-import { FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { FiArrowDown, FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const Filter = () => {
   const categories = [
@@ -18,10 +19,63 @@ const Filter = () => {
     { categoryId: 5, categoryName: "Toys" },
   ];
 
+  const [searchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate();
+
   const [category, setCategory] = useState("all");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const currentCategory = searchParams.get("category") || "all";
+    const currentSortOrder = searchParams.get("sortBy") || "asc";
+    const currentSearchTerm = searchParams.get("keyword") || "";
+
+    setCategory(currentCategory);
+    setSortOrder(currentSortOrder);
+    setSearchTerm(currentSearchTerm);
+  }, [searchParams]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (searchTerm) {
+        searchParams.set("keyword", searchTerm);
+      } else {
+        searchParams.delete("keyword");
+      }
+      navigate(`${pathname}?${searchParams.toString()}`);
+    }, 700);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchParams, searchTerm, navigate, pathname]);
 
   const handleCategopryChange = (event) => {
+    const selectedCategory = event.target.value;
+
+    if (selectedCategory === "all") {
+      params.delete("category");
+    } else {
+      params.set("category", selectedCategory);
+    }
+    navigate(`${pathname}?${params}`);
     setCategory(event.target.value);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => {
+      const newOrder = prevOrder === "asc" ? "desc" : "asc";
+      params.set("sortBy", newOrder);
+      navigate(`${pathname}?${params}`);
+      return newOrder;
+    });
+  };
+
+  const handleClearFilters = () => {
+    navigate({ pathname: window.Location.pathname });
   };
 
   return (
@@ -31,16 +85,18 @@ const Filter = () => {
         <input
           type="text"
           placeholder="Search Products"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="border border-gray-400 text-slate-800  rounded-md py-2 pl-10 pr-4 
           w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
         />
-        <FiSearch className="absolute left-3 text-slate-800 size={20}" />
+        <FiSearch className="absolute left-3 text-slate-800" size={20} />
       </div>
 
       {/* Category selections */}
       <div className="flex sm:flex-row flex-col gap-4 items-center">
         <FormControl
-          className="text-sl-800 border-slate-700"
+          className="text-slate-800 border-slate-700"
           variant="outlined"
           size="small"
         >
@@ -64,17 +120,23 @@ const Filter = () => {
         {/* Sort button & clean filter */}
         <Tooltip title="Sorted by price: asc">
           <Button
+            onClick={toggleSortOrder}
             variant="contained"
             color="primary"
             className="flex items-center gap-2 h-10"
           >
             Sort By
-            <FiArrowUp size={20} />
+            {sortOrder === "asc" ? (
+              <FiArrowUp size={20} />
+            ) : (
+              <FiArrowDown size={20} />
+            )}
           </Button>
         </Tooltip>
         <button
           className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md 
         transition duration-300 ease-in shadow-md focus:outline-none"
+          onClick={handleClearFilters}
         >
           <FiRefreshCw className="font-semibold" size={16} />
           <span className="font-semibold">Clear Filter</span>
