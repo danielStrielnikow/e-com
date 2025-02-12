0faa6ef8f4f8efe02d33a1c4e9036fe643eceeac
Daniel <daniel.strielnikow@outlook.com>
2025-02-12 12:09:21 +0100

    FIX the CORS Issue

--- e-commerce-back/src/main/java/pl/ecommerce/project/security/WebSecurityConfig.java
+++ e-commerce-back/src/main/java/pl/ecommerce/project/security/WebSecurityConfig.java
@@ -75,6 +75,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 //.requestMatchers("/api/admin/**").permitAll()
