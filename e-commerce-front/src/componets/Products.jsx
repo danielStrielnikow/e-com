63aafc0e250ad1998edcd3db8966d2e766286dd8
Daniel <daniel.strielnikow@outlook.com>
2025-02-12 20:50:50 +0100

    Fix filter

--- e-commerce-front/src/componets/Products.jsx
+++ e-commerce-front/src/componets/Products.jsx
@@ -4,18 +4,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchProducts } from "../store/actions";
 import Filter from "./Filter";
+import useProductFilter from "./useProductFilter";
 const Products = () => {
   const { isLoading, errorMessage } = useSelector((state) => state.errors);
   const { products } = useSelector((state) => state.products);
   const dispatch = useDispatch();
+  useProductFilter();
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(fetchProducts());
+//   }, [dispatch]);
 
   return (
     <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2x1:mx-auto">
-        <Filter/>
+      <Filter />
       {isLoading ? (
         <p>It is loading ...</p>
       ) : errorMessage ? (
