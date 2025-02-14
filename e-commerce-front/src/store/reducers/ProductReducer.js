9c452009cbce6a823caf4327129c5dc8cb96dc1c
Daniel <daniel.strielnikow@outlook.com>
2025-02-14 12:47:48 +0100

    Making Categories dymanic

--- e-commerce-front/src/store/reducers/ProductReducer.js
+++ e-commerce-front/src/store/reducers/ProductReducer.js
@@ -1,12 +1,33 @@
 const initialState = {
   product: null,
-  categories: null,
+  categories: [],
   pagination: {},
 };
 
 export const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_PRODUCTS":
+      return {
+        ...state,
+        products: action.payload,
+        pagination: {
+          ...state.pagination,
+          pageNumber: action.pageNumber,
+          pageSize: action.pageSize,
+          totalElements: action.totalElements,
+          totalPages: action.totalPages,
+          lastPage: action.lastPage,
+        },
+      };
+
+      case "FETCH_CATEGORIES":
+      return {
+        ...state,
+        categories: action.payload,
+        
+      };
+
+      case "FETCH_PRODUCTS":
       return {
         ...state,
         products: action.payload,
