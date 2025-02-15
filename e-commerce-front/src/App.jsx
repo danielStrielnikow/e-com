a2980664a4d7084b0579a37a1e6abfc695eb1521
Daniel <daniel.strielnikow@outlook.com>
2025-02-15 23:16:27 +0100

    Adding banner on HomePage

--- e-commerce-front/src/App.jsx
+++ e-commerce-front/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Products from './componets/products/Products'
-
+import { useState } from "react";
+import "./App.css";
+import Products from "./componets/products/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./componets/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Products />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
