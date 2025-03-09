956f3b19b7e85a8719392461d0e78a0e114ef218
Daniel <daniel.strielnikow@outlook.com>
2025-03-09 21:59:34 +0100

    Deploy app to docker

--- e-commerce-front/src/App.jsx
+++ e-commerce-front/src/App.jsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react'
-import './App.css'
-import Products from './components/products/Products'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/home/Home'
 import Navbar from './components/shared/NavBar'
-import About from './components/About'
-import Contact from './components/Contact'
 import { Toaster } from 'react-hot-toast'
-import Cart from './components/cart/Cart'
-import LogIn from './components/auth/Login'
 import PrivateRoute from './components/PrivateRoute'
-import Register from './components/auth/Register'
-import Checkout from './components/checkout/Checkout'
-import PaymentConfirmation from './components/checkout/PaymentConfirmation'
-import FavoritesList from './components/shared/FavoritesList'
+
+const Home = React.lazy(() => import('./components/home/Home'))
+const Products = React.lazy(() => import('./components/products/Products'))
+const About = React.lazy(() => import('./components/About'))
+const Contact = React.lazy(() => import('./components/Contact'))
+const Cart = React.lazy(() => import('./components/cart/Cart'))
+const LogIn = React.lazy(() => import('./components/auth/Login'))
+const Register = React.lazy(() => import('./components/auth/Register'))
+const Checkout = React.lazy(() => import('./components/checkout/Checkout'))
+const PaymentConfirmation = React.lazy(() => import('./components/checkout/PaymentConfirmation'))
+const FavoritesList = React.lazy(() => import('./components/shared/FavoritesList'))
 
 function App() {
   return (
     <React.Fragment>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' element={ <Home />}/>
-          <Route path='/products' element={ <Products />}/>
-          <Route path='/about' element={ <About />}/>
-          <Route path='/contact' element={ <Contact />}/>
-          <Route path='/list' element={ <FavoritesList />}/>
-          <Route path='/cart' element={ <Cart />}/>
-        
-          <Route path='/' element={<PrivateRoute />}>
-            <Route path='/checkout' element={ <Checkout />}/>
-            <Route path='/order-confirm' element={ <PaymentConfirmation />}/>
-          </Route>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/list' element={<FavoritesList />} />
+            <Route path='/cart' element={<Cart />} />
 
-          <Route path='/' element={<PrivateRoute publicPage />}>
-            <Route path='/login' element={ <LogIn />}/>
-            <Route path='/register' element={ <Register />}/>
-          </Route>
-        </Routes>
+            <Route path='/' element={<PrivateRoute />}>
+              <Route path='/checkout' element={<Checkout />} />
+              <Route path='/order-confirm' element={<PaymentConfirmation />} />
+            </Route>
+
+            <Route path='/' element={<PrivateRoute publicPage />}>
+              <Route path='/login' element={<LogIn />} />
+              <Route path='/register' element={<Register />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </Router>
-      <Toaster position='bottom-center'/>
+      <Toaster position='bottom-center' />
     </React.Fragment>
   )
 }
